b5f2b469c320523498d0056bcc928341f14cf841
Rodrigo Martins <mr.rodrigocarrico@gmail.com>
2025-04-11 09:50:17 +0100

    dayjs, webpack, serverjson and babel setup

---
+++ src/main.js
@@ -0,0 +1,12 @@
+/*Entry point*/
+"use strict"
+
+//Configuração do dayjs
+import "./lib/dayjs.js"
+
+//CSS
+import "./styles/global.css"
+import "./styles/utility.css"
+import "./styles/form.css"
+import "./styles/agenda.css"
+
